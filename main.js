f309908982aea99408d3d6bb1f3349ad0545cd16
Itzdlg <dontyoutrybrah@gmail.com>
2020-10-14 11:56:44 -0400

    Added search feature

--- main.js
+++ main.js
@@ -6,3 +6,44 @@ function copyURI(evt) {
 
     });
 }
+
+function search() {
+  // Declare variables
+  var table;
+  table = document.getElementsByClassName("table-wrap");
+  
+  for (i = 0; i < table.length; i++) {
+    searchTable(table[i]);
+  }
+}
+
+function searchTable(wrap) {
+  var input, filter, tr, td, i, txtValue, matches, table;
+  table = wrap.getElementsByTagName("table")[0];
+  
+  
+  input = document.getElementById("searchBox");
+  filter = input.value.toUpperCase();
+  tr = table.getElementsByTagName("tr");
+  matches = 0;
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        matches++;
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+  
+  if (matches == 0) {
+    wrap.style.display = "none";
+  } else {
+    wrap.style.display = "";
+  }
+}
